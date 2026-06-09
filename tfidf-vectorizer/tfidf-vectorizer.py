a567dad10c9da6513c4d582094ad5913ad64b84c
Nigam Niraula <nigam21nir@gmail.com>
2026-06-09 11:43:08 +0545

    Solve tfidf-vectorizer

--- tfidf-vectorizer/tfidf-vectorizer.py
+++ tfidf-vectorizer/tfidf-vectorizer.py
@@ -9,22 +9,29 @@ def tfidf_vectorizer(documents):
     """
     # Write code here
     documents =[ [el.lower() for el in doc.split()] for doc in documents]
-    
     vocab = set(el for doc in documents for el in doc)
     vocab = sorted(vocab)
-    matrix= np.zeros((len(documents),len(vocab)))
+    vocab_len= len(vocab)
+    doc_len= len(documents)
+    matrix= np.zeros((doc_len, vocab_len))
+    vocab_index = {word: i for i,word in enumerate(vocab)}
     df={}
+    document_counters=[]
+    
+    for doc in documents:
+        count_df = Counter(doc)
+        document_counters.append(count_df)
+        for key in count_df.keys():
+            df[key] = df.get(key,0) +1
+    
+    idf={}
     for word in vocab:
-        for doc in documents:
-            if word in doc:
-                if word not in df.keys():
-                    df[word] =0 
-                df[word]+=1           
+        idf[word] = np.log(doc_len / df[word])
+
     for i,doc in enumerate(documents):
-        for j,word in enumerate(vocab):
-             idf = np.log(len(documents) / df[word])
-             tf =doc.count(word) /len(doc) 
-             matrix[i][j]=  tf * idf         
-    
-    
+        count_df= document_counters[i]
+        for key,val in count_df.items():
+            tf = val/len(doc)
+            matrix[i][vocab_index[key]]= tf *idf[key]
+
     return (matrix,vocab)
