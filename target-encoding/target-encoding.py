e659ca448d645aefa826d0ab0f4f817569d0681b
Nigam Niraula <nigam21nir@gmail.com>
2026-06-16 14:00:18 +0545

    Solve target-encoding

--- target-encoding/target-encoding.py
+++ target-encoding/target-encoding.py
@@ -7,7 +7,7 @@ def target_encoding(categories, targets):
     for i,cat in enumerate(categories):
         group_count_df[cat] = group_count_df.get(cat,0)+targets[i]
         cat_count_df[cat] =  cat_count_df.get(cat,0)+1
-    cat_count_df = {cat: group_count_df[cat]/cat_count_df[cat] for cat in group_count_df}
-    return [cat_count_df[cat] for cat  in categories]
+    target_encoding= {cat: group_count_df[cat]/cat_count_df[cat] for cat in group_count_df}
+    return [target_encoding[cat] for cat  in categories]
 
     
