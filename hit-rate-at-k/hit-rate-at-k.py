efbe32b749bf62864fd2e8db113bf40d9670c5a2
Nigam Niraula <nigam21nir@gmail.com>
2026-07-07 15:28:57 +0545

    Solve hit-rate-at-k

--- hit-rate-at-k/hit-rate-at-k.py
+++ hit-rate-at-k/hit-rate-at-k.py
@@ -3,8 +3,6 @@ def hit_rate_at_k(recommendations, ground_truth, k):
     Compute the hit rate at K.
     """
     # Write code here
-    hit =0 
-
     hit = sum([1 if t[0] in recommendations[i][:k] else 0 for i,t in enumerate(ground_truth)])
 
     return hit/len(ground_truth)
