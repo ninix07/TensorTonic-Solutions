a42ab0d222636bebec3bfc467060c69662eee752
Nigam Niraula <nigam21nir@gmail.com>
2026-07-08 14:29:24 +0545

    Solve edit-distance

--- edit-distance/edit-distance.py
+++ edit-distance/edit-distance.py
@@ -7,22 +7,12 @@ def edit_distance(s1, s2):
     n= len(s2)
 
     dp = np.zeros((m+1,n+1))
-    
-    for i in range(m+1):
-        for j in range(n+1):
-            if i==0:
-                dp[i][j]=j
-                continue
-            elif j ==0:
-                dp[i][j]=i
-                continue
+    dp[0,:]= [i for i in range(0,n+1)]
+    dp[:,0]= [i for i in range(0,m+1)]
 
-            if s1[i-1]==s2[j-1]:
-                dp[i][j]= dp[i-1][j-1]
-
-            else:
-                dp[i][j] =1 + min(dp[i-1][j],dp[i][j-1],dp[i-1][j-1])
-
+    for i in range(1, m + 1):
+        for j in range(1, n + 1):
+            dp[i, j] = dp[i-1, j-1] if s1[i-1] == s2[j-1] else 1 + min(dp[i-1, j], dp[i, j-1], dp[i-1, j-1])
 
 
     return int(dp[m][n])              
