8e96910d2dc5e8450a84460f97b009b1646ac9d9
Nigam Niraula <nigam21nir@gmail.com>
2026-07-21 23:00:28 +0545

    Solve matrix-factorization-sgd-step

---
+++ matrix-factorization-sgd-step/matrix-factorization-sgd-step.py
@@ -0,0 +1,15 @@
+import numpy as np
+def matrix_factorization_sgd_step(U, V, r, lr, reg):
+    """
+    Perform one SGD step for matrix factorization.
+    """
+    # Write code here
+    U = np.asarray(U)
+    V = np.asarray(V)
+
+    e = r- np.dot(U,V)
+    U_new = U + lr*(e*V- reg*U)
+    V_new = V+lr*(e*U- reg*V)
+
+    return U_new,V_new
+    
