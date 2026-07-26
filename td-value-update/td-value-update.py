3e8f89c76f28d7acc8b12586b8f958521738f71a
Nigam Niraula <nigam21nir@gmail.com>
2026-07-26 11:38:34 +0545

    Solve td-value-update

---
+++ td-value-update/td-value-update.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+def td_value_update(V, s, r, s_next, alpha, gamma):
+    """
+    Returns: updated value function V_new
+    """
+    # Write code here
+    V_new = V.copy()
+    err = r + gamma*V[s_next] -V[s]
+    V_new[s] = V_new[s]+alpha*err
+    return V_new
