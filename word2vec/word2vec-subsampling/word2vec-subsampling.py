df5d841613bc8dce4aba466a25f557e0749e1e64
Nigam Niraula <nigam21nir@gmail.com>
2026-09-07 14:20:12 +0545

    Solve word2vec-subsampling

--- word2vec/word2vec-subsampling/word2vec-subsampling.py
+++ word2vec/word2vec-subsampling/word2vec-subsampling.py
@@ -7,4 +7,4 @@ def subsample_keep_probs(counts: torch.Tensor,
     """
     f_w: torch.Tensor = counts / torch.sum(counts)
 
-    return torch.min(torch.tensor(1.0),torch.sqrt(t/f_w))
+    return torch.clamp(torch.sqrt(t/f_w),max=1.0)
