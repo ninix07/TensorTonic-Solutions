2ef581f5e66e2eb9b2633f87a288d304994b2c13
Nigam Niraula <nigam21nir@gmail.com>
2026-09-07 14:24:50 +0545

    Solve rnn-cell

---
+++ rnn/rnn-cell/rnn-cell.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def rnn_cell(x_t: np.ndarray, h_prev: np.ndarray, W_xh: np.ndarray,
+             W_hh: np.ndarray, b_h: np.ndarray) -> np.ndarray:
+    """
+    Returns the float64 next hidden state.
+    """
+    return np.tanh(x_t@W_xh.T+h_prev@W_hh.T+b_h)
